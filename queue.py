98d156a2f8c6bcf05b991e144143f5b1ae79fd32
Hollis Akins <hollis.akins@gmail.com>
2018-08-15 20:52:03 -0400

    queue.py for night of aug 15

--- queue.py
+++ queue.py
@@ -7,36 +7,34 @@ tsx.TSXPort = 3040
 print('Attempting connection to SkyX at %s:%s' % (tsx.TSXHost,tsx.TSXPort))
 
 camera = "Imager"
-Red = "0"
-Green = "1"
-Blue = "2"
-R = "3"
-V = "4"
-B = "5"
-Halpha = "6"
-Lum = "7"
+targetIdentified = False
+while not targetIdentified:
+	target = str(raw_input('Target name: '))
+	if tsx.targExists(target)=='Yes':
+		print('Slewing to target %s' % target)
+		targetIdentified = True
+	else:
+		print('Target not found in SkyX database')
 
-numTargets = input('How many targets? ')
-targets = []
-for i in range(int(numTargets)):
-	while True:
-		aTarget = input('  %s: ' % str(i+1))
-		if tsx.targExists(aTarget)=='Yes':
-			targets.append(aTarget)
-			break
+tsx.slew(target)
+print('')
+num = str(raw_input('How many images do you want to take? '))
+print('')
+exptime = str(raw_input('Exposure time of each image in seconds? '))
+print('')
+delay = str(raw_input('Delay between each image in seconds? '))
+print('')
+print('Red = "0", Green = "1", Blue = "2", R = "3", V = "4", B = "5", Halpha = "6", Lum = "7"')
+filt = str(raw_input('Filter for each image (number)? '))
+print('')
 
-exptime = "5"
-delay = "0"
-
-
-
-for target in targets:
-	tsx.slew(target)
-	sleep(1)
-	tsx.takeImage(camera,exptime,delay,V)
-	sleep(1)
-	#tsx.getStats()
+sleep(1)
+for i in range(int(num)):
+	tsx.takeImage(camera,exptime,delay,filt)
 	print('Saved file to %s' % tsx.getActiveImagePath())
+	sleep(2)
+	path = tsx.getActiveImagePath()
+	tsx.getStatsPath(path)
 
 
 
